3cb8210e654f990a5e7e2393ecc6c399db576126
David Beazley <dave@dabeaz.com>
2015-11-01 07:19:20 -0600

    Benchmarking work

---
+++ examples/bench/asyncstream.py
@@ -0,0 +1,21 @@
+# Example: A simple echo server written using asyncio streams
+
+import asyncio
+
+async def echo_client(reader, writer):
+    print('Here!')
+    addr = writer.get_extra_info('peername')
+    print('Connection from', addr)
+    while True:
+         data = await reader.read(10000)
+         if not data:
+             break
+         writer.write(data)
+         await writer.drain()
+    print('Connection closed')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(echo_client, '127.0.0.1', 25000, loop=loop)    
+    loop.run_until_complete(coro)
+    loop.run_forever()
