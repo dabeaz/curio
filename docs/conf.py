b8d848551a3d2e77edf47acf8cc4ab8c6bd3b2ff
William Stewart <zoidbergwill@gmail.com>
2015-12-07 16:29:36 +0200

    Add intersphinx for linking to Python docs

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,11 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
