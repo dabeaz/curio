d162137e8364b6765789442a02b34fdec9d37ff8
David Beazley <dave@dabeaz.com>
2016-05-12 21:13:58 +0000

    Added gevent SSL

---
+++ examples/bench/gevsslecho.py
@@ -0,0 +1,19 @@
+from gevent.server import StreamServer
+from socket import IPPROTO_TCP, TCP_NODELAY
+
+# this handler will be run for each incoming connection in a dedicated greenlet
+def echo(socket, address):
+    print('New connection from %s:%s' % address)
+    socket.setsockopt(IPPROTO_TCP, TCP_NODELAY, 1)            
+    while True:
+        data = socket.recv(102400)
+        if not data:
+            break
+        socket.sendall(data)
+    socket.close()
+
+if __name__ == '__main__':
+    KEYFILE = "ssl_test_rsa"  
+    CERTFILE = "ssl_test.crt" 
+    server = StreamServer(('0.0.0.0', 25000), echo, keyfile=KEYFILE, certfile=CERTFILE)
+    server.serve_forever()
