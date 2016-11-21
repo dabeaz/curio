d5d7a6c10e5aa447d762cc78b9e6a465227bda3b
Alexander Zhukov <zhukovaa90@gmail.com>
2016-11-21 18:25:40 +0200

    Improve introspection for curio.Local
    Since Local doesn't have a __dict__ attribute, some introspection
    functions, like vars() and dir() either work incorrectlhy or fail.
    This commit fixes it, borrowing some approaches from threading.local

--- tests/test_local.py
+++ tests/test_local.py
@@ -17,20 +17,21 @@ def run_with_real_exceptions(*args, **kwargs):
         e.__cause__ = None
         raise real from None
 
+
 def test_smoketest():
     local = Local()
 
     async def smoketest():
-        assert local.__dict__ == {}
+        assert vars(local) == {}
         local.a = 1
         assert local.a == 1
-        assert local.__dict__ == {"a": 1}
+        assert vars(local) == {"a": 1}
         del local.a
         with pytest.raises(AttributeError):
             local.a
         with pytest.raises(AttributeError):
             del local.a
-        assert local.__dict__ == {}
+        assert vars(local) == {}
 
         local.__dict__["b"] = 2
         assert local.b == 2
