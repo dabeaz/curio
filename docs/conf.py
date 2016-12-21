fe792d2e401bb7e6b725b1400bfcfb6c27121455
Giampaolo Rodola <g.rodola@gmail.com>
2016-12-21 20:24:46 +0100

    pep8ify doc dir

--- docs/conf.py
+++ docs/conf.py
@@ -212,25 +212,25 @@ htmlhelp_basename = 'curiodoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
+    # Latex figure (float) alignment
+    #'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'curio.tex', u'curio Documentation',
-   u'David Beazley', 'manual'),
+    (master_doc, 'curio.tex', u'curio Documentation',
+     u'David Beazley', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -273,9 +273,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'curio', u'curio Documentation',
-   author, 'curio', 'One line description of project.',
-   'Miscellaneous'),
+    (master_doc, 'curio', u'curio Documentation',
+     author, 'curio', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
