aa0c2654741d877605420afddb0c4d631fd23e91
Alex Chamberlain <alex@alexchamberlain.co.uk>
2017-03-04 07:37:48 +0000

    Simplify Promise implementation by using Event.

--- curio/promise.py
+++ curio/promise.py
@@ -1,39 +1,30 @@
-from .sync import Condition
+from .sync import Event
 
 __all__ = ['Promise']
 
 class Promise:
     def __init__(self):
-        self._done = False
+        self._event = Event()
         self._data = None
         self._exception = None
 
-        self._condition = Condition()
-
     async def set(self, data):
-        async with self._condition:
-            self._data = data
-            self._done = True
-
-            await self._condition.notify()
+        self._data = data
+        await self._event.set()
 
     async def get(self):
-        async with self._condition:
-            while not self._done:
-                await self._condition.wait()
+        await self._event.wait()
 
-            if self._exception is not None:
-                raise self._exception
+        if self._exception is not None:
+            raise self._exception
 
-            return self._data
+        return self._data
 
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        async with self._condition:
-            self._done = True
-            self._exception = exc
-            await self._condition.notify()
+        self._exception = exc
+        await self._event.set()
 
         return True
