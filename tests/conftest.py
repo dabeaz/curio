cb94c342234546ec5a43a7d133d6da7710122eb7
David Beazley <dave@dabeaz.com>
2017-03-07 06:04:05 -0600

    Added collect_ignore to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import sys
 import pytest
 from curio import Kernel
 from curio.monitor import Monitor
@@ -9,3 +10,7 @@ def kernel(request):
     request.addfinalizer(lambda: k.run(shutdown=True))
     request.addfinalizer(m.close)
     return k
+
+collect_ignore = []
+if sys.version_info < (3,6):
+    collect_ignore.append("test_asyncgen.py")
