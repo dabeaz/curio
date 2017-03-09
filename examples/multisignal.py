277f6215e34821d5475e77a8af1a5b17b4a03cdf
David Beazley <dave@dabeaz.com>
2017-03-09 06:04:54 -0600

    Added example

---
+++ examples/multisignal.py
@@ -0,0 +1,49 @@
+# An example of receiving signals in curio, asyncio, and threads
+# all at once using Curio's SignalSet functionality.
+
+from curio.bridge import AsyncioLoop
+from curio import *
+import os
+import signal
+import threading
+
+async def signal_task(label):
+    async with SignalSet(signal.SIGUSR1, signal.SIGUSR2, signal.SIGINT) as s:
+        while True:
+            signo = await s.wait()
+            print(label, 'got:', signo)
+            if signo == signal.SIGINT:
+                print(label, 'Goodbye!')
+                break
+
+def signal_thread(label):
+    with SignalSet(signal.SIGUSR1, signal.SIGUSR2, signal.SIGINT) as s:
+        while True:
+            signo = s.wait()
+            print(label, 'got:', signo)
+            if signo == signal.SIGINT:
+                print(label, 'Goodbye!')
+                break
+
+async def main():
+    loop = AsyncioLoop()
+    # Launch the *same* async function in curio and in asyncio (running in different threads)
+    t1 = await spawn(signal_task, 'curio')
+    t2 = await spawn(loop.run_asyncio(signal_task, 'asyncio'))
+
+    # Launch an independent thread
+    t3 = threading.Thread(target=signal_thread, args=('thread',))
+    t3.start()
+                       
+    print('Send me signals on PID', os.getpid())
+
+    await t1.join()
+    await t2.join()
+    await run_in_thread(t3.join)
+
+if __name__ == '__main__':
+    with EnableSignals(signal.SIGUSR1, signal.SIGUSR2, signal.SIGINT) as s:
+        run(main())
+
+    
+    
