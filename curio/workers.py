6355afef735927d19bc7d36632bfe6c707064074
David Beazley <dave@dabeaz.com>
2018-04-14 11:43:38 -0500

    Fixed Issue #256. Changed run_in_process spawning method

--- curio/workers.py
+++ curio/workers.py
@@ -343,8 +343,9 @@ class ProcessWorker(object):
         self.pool = pool
 
     def _launch(self):
-        client_ch, server_ch = multiprocessing.Pipe()
-        self.process = multiprocessing.Process(
+        context = multiprocessing.get_context('spawn')
+        client_ch, server_ch = context.Pipe()
+        self.process = context.Process(
             target=self.run_server, args=(server_ch, ), daemon=True)
         self.process.start()
         server_ch.close()
