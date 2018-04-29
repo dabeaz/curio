7a442f8e91fe1a8b0318e63194dad7d2b6c366cf
David Beazley <dave@dabeaz.com>
2018-04-29 14:31:18 -0500

    moved some async thread I/O code

--- curio/io.py
+++ curio/io.py
@@ -86,7 +86,6 @@ class _Fd(object):
 # other, the KISSS (Keep it Stupid Simple Stupid) principle might be a
 # better policy--just in case someone needs to debug it.
 
-
 class Socket(object):
     '''
     Non-blocking wrapper around a socket object.   The original socket is put
@@ -633,3 +632,460 @@ class SocketStream(StreamBase):
         except errors.CancelledError as e:
             e.bytes_written = nwritten
             raise
+
+# Wherein the structure of space-time folds in upon itself and ties
+# itself into a fractal threaded knot, code beyond this point abides.
+# 
+# These classes are for performing I/O within Async Threads. 
+# Currently undocumented and experimental. Use at your own risk.
+
+class SyncSocket(object):
+
+    def __init__(self, sock):
+        self._socket = sock
+        self._socket.setblocking(False)
+        self._fileno = _Fd(sock.fileno())
+
+        # Commonly used bound methods
+        self._socket_send = sock.send
+        self._socket_recv = sock.recv
+
+    def __repr__(self):
+        return '<curio.SyncSocket %r>' % (self._socket)
+
+    def __getattr__(self, name):
+        return getattr(self._socket, name)
+
+    def fileno(self):
+        return int(self._fileno)
+
+    def settimeout(self, seconds):
+        if seconds:
+            raise RuntimeError('Use timeout_after() to set a timeout')
+
+    def gettimeout(self):
+        return None
+
+    def dup(self):
+        return type(self)(self._socket.dup())
+
+    def makefile(self, mode, buffering=0, *, encoding=None, errors=None, newline=None):
+        if 'b' not in mode:
+            raise RuntimeError('File can only be created in binary mode')
+        f = self._socket.makefile(mode, buffering=buffering)
+        return SyncFileStream(f)
+
+    def as_stream(self):
+        return SyncSocketStream(self._socket)
+
+    def recv(self, maxsize, flags=0):
+        while True:
+            try:
+                return self._socket_recv(maxsize, flags)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def recv_into(self, buffer, nbytes=0, flags=0):
+        while True:
+            try:
+                return self._socket.recv_into(buffer, nbytes, flags)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def send(self, data, flags=0):
+        while True:
+            try:
+                return self._socket_send(data, flags)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+            except WantRead: 
+                thread.AWAIT(_read_wait, self._fileno)
+
+    def sendall(self, data, flags=0):
+        with memoryview(data).cast('B') as buffer:
+            total_sent = 0
+            try:
+                while buffer:
+                    try:
+                        nsent = self._socket_send(buffer, flags)
+                        total_sent += nsent
+                        buffer = buffer[nsent:]
+                    except WantWrite:
+                        thread.AWAIT(_write_wait, self._fileno)
+                    except WantRead: 
+                        thread.AWAIT(_read_wait, self._fileno)
+            except curio.errors.CancelledError as e:
+                e.bytes_sent = total_sent
+                raise
+
+    def accept(self):
+        while True:
+            try:
+                client, addr = self._socket.accept()
+                return type(self)(client), addr
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+
+    def connect_ex(self, address):
+        try:
+            self.connect(address)
+            return 0
+        except OSError as e:
+            return e.errno
+
+    def connect(self, address):
+        try:
+            result = self._socket.connect(address)
+            if getattr(self, 'do_handshake_on_connect', False):
+                self.do_handshake()
+            return result
+        except WantWrite:
+            thread.AWAIT(_write_wait, self._fileno)
+        err = self._socket.getsockopt(SOL_SOCKET, SO_ERROR)
+        if err != 0:
+            raise OSError(err, 'Connect call failed %s' % (address,))
+        if getattr(self, 'do_handshake_on_connect', False):
+            self.do_handshake()
+
+    def recvfrom(self, buffersize, flags=0):
+        while True:
+            try:
+                return self._socket.recvfrom(buffersize, flags)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def recvfrom_into(self, buffer, bytes=0, flags=0):
+        while True:
+            try:
+                return self._socket.recvfrom_into(buffer, bytes, flags)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def sendto(self, bytes, flags_or_address, address=None):
+        if address:
+            flags = flags_or_address
+        else:
+            address = flags_or_address
+            flags = 0
+        while True:
+            try:
+                return self._socket.sendto(bytes, flags, address)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+
+    def recvmsg(self, bufsize, ancbufsize=0, flags=0):
+        while True:
+            try:
+                return self._socket.recvmsg(bufsize, ancbufsize, flags)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+
+    def recvmsg_into(self, buffers, ancbufsize=0, flags=0):
+        while True:
+            try:
+                return self._socket.recvmsg_into(buffers, ancbufsize, flags)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+
+    def sendmsg(self, buffers, ancdata=(), flags=0, address=None):
+        while True:
+            try:
+                return self._socket.sendmsg(buffers, ancdata, flags, address)
+            except WantRead:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    # Special functions for SSL
+    def do_handshake(self):
+        while True:
+            try:
+                return self._socket.do_handshake()
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def close(self):
+        if self._socket:
+            self._socket.close()
+        self._socket = None
+        self._fileno = -1
+
+    def shutdown(self, how):
+        if self._socket:
+            self._socket.shutdown(how)
+
+class SyncStreamBase(object):
+
+    def __init__(self, fileobj):
+        self._file = fileobj
+        self._fileno = _Fd(fileobj.fileno())
+        self._buffer = bytearray()
+
+    def __repr__(self):
+        return '<curio.%s %r>' % (type(self).__name__, self._file)
+
+    def fileno(self):
+        return int(self._fileno)
+
+    # ---- Methods that must be implemented in child classes
+    def _read(self, maxbytes=-1):   
+        raise NotImplementedError()
+
+    def write(self, data):          
+        raise NotImplementedError()
+
+    # ---- General I/O methods for streams
+    def read(self, maxbytes=-1):
+        buf = self._buffer
+        if buf:
+            if maxbytes < 0 or len(buf) <= maxbytes:
+                data = bytes(buf)
+                buf.clear()
+            else:
+                data = bytes(buf[:maxbytes])
+                del buf[:maxbytes]
+        else:
+            data = self._read(maxbytes)
+        return data
+
+    def readall(self):
+        chunks = []
+        maxread = 65536
+        if self._buffer:
+            chunks.append(bytes(self._buffer))
+            self._buffer.clear()
+        while True:
+            try:
+                chunk = self.read(maxread)
+            except curio.errors.CancelledError as e:
+                e.bytes_read = b''.join(chunks)
+                raise
+            if not chunk:
+                return b''.join(chunks)
+            chunks.append(chunk)
+            if len(chunk) == maxread:
+                maxread *= 2
+
+    def read_exactly(self, nbytes):
+        chunks = []
+        while nbytes > 0:
+            try:
+                chunk = self.read(nbytes)
+            except curio.errors.CancelledError as e:
+                e.bytes_read = b''.join(chunks)
+                raise
+            if not chunk:
+                e = EOFError('Unexpected end of data')
+                e.bytes_read = b''.join(chunks)
+                raise e
+            chunks.append(chunk)
+            nbytes -= len(chunk)
+        return b''.join(chunks)
+
+    def readinto(self, memory):
+        with memoryview(memory).cast('B') as view:
+            remaining = len(view)
+            total_read = 0
+
+            # It's possible that there is data already buffered on this stream. 
+            # If so, we have to copy into the memory buffer first.
+            buffered = len(self._buffer)
+            tocopy = remaining if (remaining < buffered) else buffered
+            if tocopy:
+                view[:tocopy] = self._buffer[:tocopy]
+                del self._buffer[:tocopy]
+                remaining -= tocopy
+                total_read += tocopy
+
+            # To emulate behavior of synchronous readinto(), we read all available
+            # bytes up to the buffer size.
+            while remaining > 0:
+                try:
+                    nrecv = self._readinto_impl(view[total_read:total_read+remaining])
+
+                    # On proper file objects, None might be returned to indicate blocking
+                    if nrecv is None:
+                        thread.AWAIT(_read_wait, self._fileno)
+                    elif nrecv == 0:
+                        break
+                    else:
+                        total_read += nrecv
+                        remaining -= nrecv
+                except WantRead:
+                    thread.AWAIT(_read_wait, self._fileno)
+                except WantWrite:
+                    thread.AWAIT(_write_wait, self._fileno)
+            return total_read
+        
+    def readline(self):
+        while True:
+            nl_index = self._buffer.find(b'\n')
+            if nl_index >= 0:
+                resp = bytes(self._buffer[:nl_index + 1])
+                del self._buffer[:nl_index + 1]
+                return resp
+            data = self._read(MAX_READ)
+            if data == b'':
+                resp = bytes(self._buffer)
+                self._buffer.clear()
+                return resp
+            self._buffer.extend(data)
+
+    def readlines(self):
+        lines = []
+        try:
+            for line in self:
+                lines.append(line)
+            return lines
+        except curio.errors.CancelledError as e:
+            e.lines_read = lines
+            raise
+
+    def writelines(self, lines):
+        nwritten = 0
+        for line in lines:
+            try:
+                self.write(line)
+                nwritten += len(line)
+            except curio.errors.CancelledError as e:
+                e.bytes_written += nwritten
+                raise
+
+    def flush(self):
+        pass
+
+    def close(self):
+        self.flush()
+        if self._file:
+            self._file.close()
+        self._file = None
+        self._fileno = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = self.readline()
+        if line:
+            return line
+        else:
+            raise StopIteration
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return self.close()
+
+
+class SyncFileStream(SyncStreamBase):
+
+    def __init__(self, fileobj):
+        assert not isinstance(fileobj, io.TextIOBase), 'Only binary mode files allowed'
+        super().__init__(fileobj)
+        os.set_blocking(int(self._fileno), False)
+
+        # Common bound methods
+        self._file_read = fileobj.read
+        self._readinto_impl = getattr(fileobj, 'readinto', None)
+        self._file_write = fileobj.write
+
+    def _read(self, maxbytes=-1):
+        while True:
+            # In non-blocking mode, a file-like object might return None if no data is
+            # available.  Alternatively, we'll catch the usual blocking exceptions just to be safe
+            try:
+                data = self._file_read(maxbytes)
+                if data is None:
+                    thread.AWAIT(_read_wait, self._fileno)
+                else:
+                    return data
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite: 
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def write(self, data):
+        nwritten = 0
+        view = memoryview(data).cast('B')
+        try:
+            while view:
+                try:
+                    nbytes = self._file_write(view)
+                    if nbytes is None:
+                        raise BlockingIOError()
+                    nwritten += nbytes
+                    view = view[nbytes:]
+                except WantWrite as e:
+                    if hasattr(e, 'characters_written'):
+                        nwritten += e.characters_written
+                        view = view[e.characters_written:]
+                    thread.AWAIT(_write_wait, self._fileno)
+                except WantRead:
+                    thread.AWAIT(_read_wait, self._fileno)
+            return nwritten
+
+        except curio.errors.CancelledError as e:
+            e.bytes_written = nwritten
+            raise
+
+    def flush(self):
+        if not self._file:
+            return
+        while True:
+            try:
+                return self._file.flush()
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+
+class SyncSocketStream(SyncStreamBase):
+
+    def __init__(self, sock):
+        super().__init__(sock)
+        sock.setblocking(False)
+
+        # Common bound methods
+        self._socket_recv = sock.recv
+        self._readinto_impl = sock.recv_into
+        self._socket_send = sock.send
+
+    def _read(self, maxbytes=-1):
+        while True:
+            try:
+                data = self._socket_recv(maxbytes if maxbytes > 0 else MAX_READ)
+                return data
+            except WantRead:
+                thread.AWAIT(_read_wait, self._fileno)
+            except WantWrite:
+                thread.AWAIT(_write_wait, self._fileno)
+
+    def write(self, data):
+        nwritten = 0
+        view = memoryview(data).cast('B')
+        try:
+            while view:
+                try:
+                    nbytes = self._socket_send(view)
+                    nwritten += nbytes
+                    view = view[nbytes:]
+                except WantWrite:
+                    thread.AWAIT(_write_wait, self._fileno)
+                except WantRead:
+                    thread.AWAIT(_read_wait, self._fileno)
+            return nwritten
+        except curio.errors.CancelledError as e:
+            e.bytes_written = nwritten
+            raise
+
