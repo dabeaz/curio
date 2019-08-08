20f90dacfcf09563b13a6c067905e540db83d8dc
David Beazley <dave@dabeaz.com>
2019-08-07 19:28:35 -0500

    Consolidated traps to a single coroutine entry point

--- curio/traps.py
+++ curio/traps.py
@@ -1,13 +1,12 @@
 # traps.py
 #
-# Coroutines corresponding to the kernel traps.  These functions
-# provide the bridge from tasks to the underlying kernel. This is the
-# only place where the explicit @coroutine decorator needs to be used.
-# All other code in curio and in user-tasks should use async/await
-# instead.  Direct use by users is allowed, but if you're working with
-# these traps directly, there is probably a higher level interface
-# that simplifies the problem you're trying to solve (e.g., Socket,
-# File, objects, etc.).
+# Curio programs execute under the supervision of a kernel. Communication
+# with the kernel takes place via a "trap" involving the yield statement.
+# Traps encode an internel kernel procedure name as well as its arguments.
+# Direct use of the functions defined here is allowed when making new
+# kinds of Curio primitives, but if you're trying to solve a higher
+# level problem, there is probably a higher-level interface that is
+# easier to use (e.g., Socket, File, Queue, etc.).
 # ----------------------------------------------------------------------
 
 __all__ = [ 
@@ -45,120 +44,108 @@ class Traps(IntEnum):
 
 globals().update((trap.name, trap) for trap in Traps)
 
-
+# This is the only entry point to the Curio kernel and the
+# only place where the @types.coroutine decorator is used. 
 @coroutine
-def _read_wait(fileobj):
+def _kernel_trap(*request):
+    return (yield request)
+
+# Higher-level trap functions that make use of async/await
+async def _read_wait(fileobj):
     '''
     Wait until reading can be performed.  If another task is waiting
     on the same file, a ResourceBusy exception is raised. 
     '''
-    yield (_trap_io, fileobj, EVENT_READ, 'READ_WAIT')
+    return await _kernel_trap(_trap_io, fileobj, EVENT_READ, 'READ_WAIT')
 
-
-@coroutine
-def _write_wait(fileobj):
+async def _write_wait(fileobj):
     '''
     Wait until writing can be performed. If another task is waiting
     to write on the same file, a ResourceBusy exception is raised.
     '''
-    yield (_trap_io, fileobj, EVENT_WRITE, 'WRITE_WAIT')
+    return await _kernel_trap(_trap_io, fileobj, EVENT_WRITE, 'WRITE_WAIT')
 
-@coroutine
-def _io_waiting(fileobj):
+async def _io_waiting(fileobj):
     '''
     Return a tuple (rtask, wtask) of tasks currently blocked waiting
     for I/O on fileobj.
     '''
-    return (yield (_trap_io_waiting, fileobj))
+    return await _kernel_trap(_trap_io_waiting, fileobj)
 
-@coroutine
-def _future_wait(future, event=None):
+async def _future_wait(future, event=None):
     '''
     Wait for the result of a Future to be ready.
     '''
-    yield (_trap_future_wait, future, event)
+    return await _kernel_trap(_trap_future_wait, future, event)
 
-
-@coroutine
-def _sleep(clock, absolute):
+async def _sleep(clock, absolute):
     '''
     Sleep until the monotonic clock reaches the specified clock value.
     If clock is 0, forces the current task to yield to the next task (if any).
     absolute is a boolean flag that indicates whether or not the clock
     period is an absolute time or relative.
     '''
-    return (yield (_trap_sleep, clock, absolute))
+    return await _kernel_trap(_trap_sleep, clock, absolute)
 
 
-@coroutine
-def _spawn(coro):
+async def _spawn(coro):
     '''
     Create a new task. Returns the resulting Task object.
     '''
-    return (yield _trap_spawn, coro)
+    return await _kernel_trap(_trap_spawn, coro)
 
 
-@coroutine
-def _cancel_task(task, exc=errors.TaskCancelled, val=None):
+async def _cancel_task(task, exc=errors.TaskCancelled, val=None):
     '''
     Cancel a task. Causes a CancelledError exception to raise in the task.
     Set the exc and val arguments to change the exception.
     '''
-    yield (_trap_cancel_task, task, exc, val)
+    return await _kernel_trap(_trap_cancel_task, task, exc, val)
 
 
-@coroutine
-def _scheduler_wait(sched, state):
+async def _scheduler_wait(sched, state):
     '''
     Put the task to sleep on a scheduler primitive.
     '''
-    yield (_trap_sched_wait, sched, state)
+    return await _kernel_trap(_trap_sched_wait, sched, state)
 
 
-@coroutine
-def _scheduler_wake(sched, n=1):
+async def _scheduler_wake(sched, n=1):
     '''
     Reschedule one or more tasks waiting on a scheduler primitive.
     '''
-    yield (_trap_sched_wake, sched, n)
+    return await _kernel_trap(_trap_sched_wake, sched, n)
 
 
-@coroutine
-def _get_kernel():
+async def _get_kernel():
     '''
     Get the kernel executing the task.
     '''
-    return (yield (_trap_get_kernel,))
+    return await _kernel_trap(_trap_get_kernel)
 
 
-@coroutine
-def _get_current():
+async def _get_current():
     '''
     Get the currently executing task
     '''
-    return (yield (_trap_get_current,))
+    return await _kernel_trap(_trap_get_current)
 
-
-@coroutine
-def _set_timeout(clock):
+async def _set_timeout(clock):
     '''
     Set a timeout for the current task that occurs at the specified clock value.
     Setting a clock of None clears any previous timeout.
     '''
-    return (yield (_trap_set_timeout, clock))
+    return await _kernel_trap(_trap_set_timeout, clock)
 
 
-@coroutine
-def _unset_timeout(previous):
+async def _unset_timeout(previous):
     '''
     Restore the previous timeout for the current task.
     '''
-    return (yield (_trap_unset_timeout, previous))
+    return await _kernel_trap(_trap_unset_timeout, previous)
 
-
-@coroutine
-def _clock():
+async def _clock():
     '''
     Return the value of the kernel clock
     '''
-    return (yield (_trap_clock,))
+    return await _kernel_trap(_trap_clock)
