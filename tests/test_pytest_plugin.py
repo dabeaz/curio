ee27c4515759616747b9991857169924fdb52824
David Beazley <dave@dabeaz.com>
2020-08-21 10:55:46 -0500

    Removed pytest plugin

--- tests/test_pytest_plugin.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-import curio
-
-
-@pytest.mark.filterwarnings('error::pytest.PytestUnhandledCoroutineWarning')
-@pytest.mark.curio
-async def test_coroutine_test():
-    await curio.sleep(0)
