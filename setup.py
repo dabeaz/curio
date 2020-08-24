1ea56538a06a51653e21874e81d4a32fd66ecf64
David Beazley <dave@dabeaz.com>
2020-08-24 04:39:28 -0500

    Reduced requirements

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name="curio",
       extras_require={
           'test': tests_require,
       },
-      python_requires='>= 3.7',
+      python_requires='>= 3.6',
       # This is disabled because it often causes interference with other testing
       # plugins people have written.  Curio doesn't use it for it's own testing.
       # entry_points={"pytest11": ["curio = curio.pytest_plugin"]},
